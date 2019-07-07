a33aca1bec6ad4c12bc9f841934b6baff1cec6c4
VladSimeonidi <simeonidiv@gmail.com>
2019-07-07 12:09:52 +0300

    exam

---
+++ js.js
@@ -0,0 +1,19 @@
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4500,
+  arrows: false,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+$('.lightbox').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: true,
+  infinite: false
+});
